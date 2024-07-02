8d8d872486df2fd0938eb5566c5a9e5cecdee77b
s1f102100704 <s1f102100704@iniad.org>
2024-07-02 11:21:29 +0900

    修正

--- server/api/openai.ts
+++ server/api/openai.ts
@@ -1,12 +1,11 @@
 import OpenAI from 'openai';
-import { main } from '../domain/novel/useCase/novelUseCase';
 const openai = new OpenAI({
-  apiKey: 'YOUR API KEY', // process.env["OPENAI_API_KEY"]
+  apiKey: process.env['OPENAI_API_KEY'],
   dangerouslyAllowBrowser: true, // ***注意*** クライアントサイドの実行を許可
 });
-export async function ask(): Promise<string> {
+export async function ask(message: string): Promise<string> {
   // メッセージを送信
-  const content = main();
+  const content = message;
   const completion = await openai.chat.completions.create({
     messages: [{ role: 'user', content }],
     model: 'gpt-3.5-turbo',
