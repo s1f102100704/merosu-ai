b21bc556b05213c3d22ee3f61d02a9f63169f4ff
s1f102100704 <s1f102100704@iniad.org>
2024-07-05 19:37:00 +0900

    Revert "修正"
    This reverts commit 8d8d872486df2fd0938eb5566c5a9e5cecdee77b.

--- server/api/openai.ts
+++ server/api/openai.ts
@@ -1,11 +1,12 @@
 import OpenAI from 'openai';
+import { main } from '../domain/novel/useCase/novelUseCase';
 const openai = new OpenAI({
-  apiKey: process.env['OPENAI_API_KEY'],
+  apiKey: 'YOUR API KEY', // process.env["OPENAI_API_KEY"]
   dangerouslyAllowBrowser: true, // ***注意*** クライアントサイドの実行を許可
 });
-export async function ask(message: string): Promise<string> {
+export async function ask(): Promise<string> {
   // メッセージを送信
-  const content = message;
+  const content = main();
   const completion = await openai.chat.completions.create({
     messages: [{ role: 'user', content }],
     model: 'gpt-3.5-turbo',
