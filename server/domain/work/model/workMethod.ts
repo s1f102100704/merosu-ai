39b3941e6a18aa87648d59b7577253436ed6d9e2
s1f102100704 <s1f102100704@iniad.org>
2024-07-06 13:04:22 +0900

    XLL

--- server/domain/work/model/workMethod.ts
+++ server/domain/work/model/workMethod.ts
@@ -2,14 +2,36 @@ import type { CompletedWorkEntity, FailedWorkEntity, LoadingWorkEntity } from 'a
 import { brandedId } from 'service/brandedId';
 import { s3 } from 'service/s3Client';
 import { ulid } from 'ulid';
+import * as yup from 'yup';
 import { getContentKey, getImageKey } from '../service/getS3Key';
 
+const aozoraUrl = yup
+  .string()
+  .test(
+    'is-aozora-url',
+    'URL must start with "aozora"',
+    (value) => typeof value === 'string' && value.startsWith('aozora'),
+  );
+
+const workSchema = yup.object().shape({
+  novelUrl: aozoraUrl.required(),
+  title: yup.string().max(255).required(),
+  author: yup.string().max(255).required(),
+});
 export const workMethod = {
   create: async (val: {
     novelUrl: string;
     title: string;
     author: string;
   }): Promise<LoadingWorkEntity> => {
+    try {
+      await workSchema.validate(val);
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        throw new Error(`Validation failed: ${error.message}`);
+      }
+      throw new Error('An unexpected error occurred during validation.');
+    }
     const id = brandedId.work.entity.parse(ulid());
     return {
       id,
