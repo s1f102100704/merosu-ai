68a3f72f1efca1168eb884e2a9f05efb54e03b53
s1f102100704 <s1f102100704@iniad.org>
2024-08-05 16:11:06 +0900

    ; => ,

--- server/api/private/works/controller.ts
+++ server/api/private/works/controller.ts
@@ -4,9 +4,7 @@ import { prismaClient } from 'service/prismaClient';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  get: () =>
-    workQuery.listAll(prismaClient).then((works) => ({ status: 200, body: works }));
-
+  get: () => workQuery.listAll(prismaClient).then((works) => ({ status: 200, body: works })),
 
   post: ({ body }) =>
     workUseCase.create(body.novelUrl).then((work) => ({ status: 200, body: work })),
